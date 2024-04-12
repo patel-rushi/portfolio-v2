b1c14afbd8c63ef1e813ab3b76b7c67299712240
Rushi Patel <mail.rushipatel@gmail.com>
2024-04-11 20:40:23 -0400

    adding logic for fetching blog from atom link

--- app/controllers/dairy_controller.rb
+++ app/controllers/dairy_controller.rb
@@ -1,4 +1,25 @@
+require 'open-uri'
+require 'nokogiri'
+
 class DairyController < ApplicationController
-    def index
+  def index
+    @blog_posts = fetch_blog_posts
+  end
+
+  private
+
+  def fetch_blog_posts
+    url = 'https://world.hey.com/rushi.patel/feed.atom'
+    document = Nokogiri::XML(URI.open(url))
+    document.remove_namespaces!
+
+    document.xpath('//entry').map do |entry|
+      {
+        title: entry.xpath('title').text,
+        category: 'Dairy',
+        url: entry.at_xpath('link[@rel="alternate"]')['href'],
+        published_at: entry.xpath('published').text
+      }
     end
+  end
 end
