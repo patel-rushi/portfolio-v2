1d5c2ba0337cfe732a5a947c47ec385346508f51
Rushi Patel <mail.rushipatel@gmail.com>
2024-05-11 02:44:10 -0400

    embeding iframe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,13 @@ class HomeController < ApplicationController
     Firstly, we immerse ourselves in the client's perspective, anticipating their needs. We delve into how they utilize the software, the type of data they handle, and the origin of their inventory management needs. Do they require a comprehensive system or a streamlined solution? Understanding their workflow helps us determine optimal placements for features and gauge complexity."
 
     @home_blog_posts = [{
+      id: 'home-blog-post',
       title: 'Transform clients\' requirements into features with minimal hassle',
       category: 'Tech',
       url: 'https://www.linkedin.com/posts/rushipatel-_design-projectmanagement-softwaredevelopment-activity-7173534615876292608-Kmea?utm_source=share&utm_medium=member_desktop',
       published_at: 'Apr 05, 2024',
       content: content.squish.truncate(200, omission: ' . . . .'),
+      embed: '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7173534613187846144" height="1200" width="100%" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>', 
     }]
   end
 end
