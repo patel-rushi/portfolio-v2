a8cc87bc3bdcf31d84b746397b220471e76f2bc1
Rushi Patel <mail.rushipatel@gmail.com>
2024-05-12 19:28:18 -0400

    extracting linkedin url too

--- app/controllers/tech_controller.rb
+++ app/controllers/tech_controller.rb
@@ -12,15 +12,18 @@ class TechController < ApplicationController
     def fetch_tech_posts
         fetch_feed_entries('https://world.hey.com/rushi.patel/feed.atom').map do |entry|
           next unless entry.xpath('title').text.include?('[Tech]')
+          embed_iframe, post_url = extract_iframe_url(entry)
           parse_entry(entry).merge(
-            category: 'Tech', 
+            category: embed_iframe.present? ? 'Linkedin Post' : 'Tech', 
             title: entry.xpath('title').text.gsub('[Tech]', '').strip, 
-            embed: extract_iframe(entry)
+            embed: embed_iframe,
+            content: embed_iframe.present? ? 'Press to checkout post' :  entry.xpath('content').text,
+            url: embed_iframe.present? ? post_url : ''
           )
         end.compact
     end
 
-    def extract_iframe(entry)
+    def extract_iframe_url(entry)
         content_encoded = entry.xpath('content').text
         content_decoded_once = CGI.unescapeHTML(content_encoded)
         content_fully_decoded = CGI.unescapeHTML(content_decoded_once)
@@ -35,9 +38,16 @@ class TechController < ApplicationController
         height_match = height_regex.match(content_fully_decoded)
         iframe_height = height_match[1] if height_match
 
+        # Extract the linkedIn post url:
+        additional_url_regex = /url="[^"]+href="([^"]+)"/
+        additional_url_match = additional_url_regex.match(content_fully_decoded)
+        post_url = additional_url_match[1] if additional_url_match
+
         # Construct the iframe tag with the extracted URL
         iframe_html = if iframe_url
                         "<iframe src=\"#{iframe_url}\" height=\"#{iframe_height || '1000'}\" width=\"100%\" frameborder=\"0\" allowfullscreen title=\"Embedded post\"></iframe>"  
                     end
+
+        [iframe_html, post_url]
     end
 end
