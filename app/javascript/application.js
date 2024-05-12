7478bc036655eee0510c889fc595d91469dc047c
Rushi Patel <mail.rushipatel@gmail.com>
2024-05-12 17:05:06 -0400

    fix import error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import { showPopup, hidePopup } from "./popup"
+import { showPopup, hidePopup } from "popup"
 
 
 function attachEventListeners() {
