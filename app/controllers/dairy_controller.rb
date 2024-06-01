21970d21312b48b03d9465dca3e1fcb7bb3cf313
Rushi Patel <mail.rushipatel@gmail.com>
2024-06-01 18:09:28 -0400

    fetching posts from page-2,3 in hey world

--- app/controllers/dairy_controller.rb
+++ app/controllers/dairy_controller.rb
@@ -11,9 +11,15 @@ class DairyController < ApplicationController
   private
 
   def fetch_dairy_posts
-    fetch_feed_entries('https://world.hey.com/rushi.patel/feed.atom').map do |entry|
-      next if entry.xpath('title').text.include?('[Tech]')
-      parse_entry(entry).merge(category: 'Diary')
-    end.compact
+    all_entries = []
+    PAGE_PARAMS.each do |page_param|
+        page_url = "#{BASE_URL}#{page_param}"
+        entries = fetch_feed_entries(page_url).map do |entry|
+          next if entry.xpath('title').text.include?('[Tech]')
+          parse_entry(entry).merge(category: 'Diary')
+        end.compact
+        all_entries.concat(entries)
+    end
+    all_entries
   end
 end
