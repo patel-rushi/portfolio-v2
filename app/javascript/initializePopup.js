cb263e59d0e64a01ef1fe0d620ea77593c4764d8
Rushi Patel <mail.rushipatel@gmail.com>
2024-06-09 17:49:21 -0400

    scroll into view and highlight

--- app/javascript/initializePopup.js
+++ app/javascript/initializePopup.js
@@ -6,6 +6,11 @@ export function initializePopup(openPostId) {
         document.addEventListener('DOMContentLoaded', () => {
             console.log(`popup-${openPostId}`);
             showPopup(openPostId);
+            var card = document.querySelector(`[data-post-id="${openPostId}"]`);
+            if (card) {
+                card.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                card.classList.add('extra-border-highlight');
+            }
         });
         sendGtagEvent('views_per_page', 'page_view', `tech/${openPostId}`);
     }
