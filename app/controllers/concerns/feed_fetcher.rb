9094710a3ba60b25241e93289929a7e12369a988
Rushi Patel <mail.rushipatel@gmail.com>
2024-06-09 18:01:40 -0400

    too late but securing urls

--- app/controllers/concerns/feed_fetcher.rb
+++ app/controllers/concerns/feed_fetcher.rb
@@ -3,13 +3,9 @@ module FeedFetcher
     
     private
     included do
-        BASE_URL = 'https://world.hey.com/rushi.patel/feed.atom'.freeze
-        PAGE_PARAMS = [
-            '', # First page doesn't have a page parameter
-            '?page=eyJwYWdlX251bWJlciI6MiwidmFsdWVzIjp7ImNyZWF0ZWRfYXQiOiIyMDI0LTAxLTIyVDAyOjE0OjU0LjE2MTQzMloiLCJpZCI6MzQ1OTN9fQ%3D%3D',
-            '?page=eyJwYWdlX251bWJlciI6MywidmFsdWVzIjp7ImNyZWF0ZWRfYXQiOiIyMDIzLTExLTI4VDIyOjAxOjE2LjQwODU1OVoiLCJpZCI6MzMwMzF9fQ%3D%3D'
-            # Add more pages as needed
-        ].freeze
+        # Update both development and produdction credentials
+        BASE_URL = Rails.application.credentials.dig(:feed, :base_url).freeze
+        PAGE_PARAMS = Rails.application.credentials.dig(:feed, :page_params).freeze
     end
 
     def fetch_feed_entries(url)
