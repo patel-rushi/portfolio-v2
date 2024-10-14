5a2e297fcf2603afd08b1a662b4d2393b5ea6e07
Rushi Patel <mail.rushipatel@gmail.com>
2024-10-14 18:48:20 -0400

    fetch diary post based on filter

--- app/controllers/dairy_controller.rb
+++ app/controllers/dairy_controller.rb
@@ -5,22 +5,7 @@ class DairyController < ApplicationController
   include FeedFetcher
   
   def index
-    @blog_posts = fetch_dairy_posts
+    @blog_posts = fetch_posts(params[:page])
     @open_post_id = params[:post]
-  end
-
-  private
-
-  def fetch_dairy_posts
-    all_entries = []
-    PAGE_PARAMS.each do |page_param|
-        page_url = "#{BASE_URL}#{page_param}"
-        entries = fetch_feed_entries(page_url).map do |entry|
-          next if entry.xpath('title').text.include?('[Tech]')
-          parse_entry(entry).merge(category: 'Diary')
-        end.compact
-        all_entries.concat(entries)
-    end
-    all_entries
   end
 end
