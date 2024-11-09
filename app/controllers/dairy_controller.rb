0cd60fd0fa70c5665486bfe88962678b22605a15
Rushi Patel <mail.rushipatel@gmail.com>
2024-11-08 19:34:30 -0500

    fetching filter names from a constant

--- app/controllers/dairy_controller.rb
+++ app/controllers/dairy_controller.rb
@@ -3,9 +3,11 @@ require 'nokogiri'
 
 class DairyController < ApplicationController
   include FeedFetcher
+  include FeedFilters
   
   def index
-    @blog_posts = fetch_posts(params[:page])
+    @page = params[:page].presence_in(FeedFilters::NAMES.keys.map(&:to_s)) || FeedFilters::NAMES.keys.first.to_s
+    @blog_posts = fetch_posts(@page) || []
     @open_post_id = params[:post]
   end
 end
