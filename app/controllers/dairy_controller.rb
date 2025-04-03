e65b29dd6c156ce5b3555bcf826f1474231d6813
Rushi Patel <mail.rushipatel@gmail.com>
2025-04-03 16:56:02 -0400

    logic update in views and controller

--- app/controllers/dairy_controller.rb
+++ app/controllers/dairy_controller.rb
@@ -2,12 +2,11 @@ require 'open-uri'
 require 'nokogiri'
 
 class DairyController < ApplicationController
-  include FeedFetcher
   include FeedFilters
   
   def index
     @page = params[:page].presence_in(FeedFilters::NAMES.keys.map(&:to_s)) || FeedFilters::NAMES.keys.first.to_s
-    @blog_posts = fetch_posts(@page) || []
+    @blog_posts = Post.where(category: @page) || []
     @open_post_id = params[:post]
   end
 end
--- app/controllers/tech_controller.rb
+++ app/controllers/tech_controller.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 class TechController < ApplicationController
-    include FeedFetcher
-
+    include FeedFilters
+    
     def index
-        @tech_posts = fetch_posts('tech')
+        @tech_posts = Post.where(category: FeedFilters::NAMES[:tech])
         @open_post_id = params[:post]
     end
 end
