5a25ee25745ac94f079a9559f054ebccbf084823
Rushi Patel <mail.rushipatel@gmail.com>
2025-04-04 22:27:14 -0400

    auth accessed features

--- app/controllers/tech_controller.rb
+++ app/controllers/tech_controller.rb
@@ -4,7 +4,7 @@ class TechController < ApplicationController
     include FeedFilters
     
     def index
-        @tech_posts = Post.where(category: FeedFilters::NAMES[:tech]).order('published_at DESC, created_at DESC')
+        @tech_posts = Post.where(category: FeedFilters::NAMES[:tech]).order('published_at DESC')
         @open_post_id = params[:post]
     end
 end
