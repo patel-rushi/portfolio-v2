61da67679db0f2a01d8d781ee59eab42e7a89e02
Rushi Patel <mail.rushipatel@gmail.com>
2025-04-06 18:19:39 -0400

    changing db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_04_04_155236) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "category"
     t.string "title"
@@ -25,11 +28,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_04_155236) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
   end
 
 end
